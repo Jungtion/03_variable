f988f9c1a95eb9022b9b965426db4c086306b655
Jungtion <lavenoey@sookmyung.ac.kr>
2019-09-19 16:09:10 +0900

    project3

--- main.cpp
+++ main.cpp
@@ -2,17 +2,18 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+int main(void) {
 	
-	int x		;
-	short y		;
-	float z		;
-	double p	;
+	int input_int		;
+	float input_float	;
 	
-	printf("bytes of int : %d\n", sizeof(x))		;
-	printf("bytes of short : %d\n", sizeof(y))		;
-	printf("bytes of float : %d\n", sizeof(z))		;
-	printf("bytes of double : %d\n", sizeof(p))		;
+	printf("enter an integer : ")	;
+	scanf("%d", &input_int)			;
+	
+	printf("enter a float : ")		;
+	scanf("%f", &input_float)		;
+	
+	printf("interger : %d, float : %f\n", input_int, input_float)	;	
 	
 	return 0;
 }
